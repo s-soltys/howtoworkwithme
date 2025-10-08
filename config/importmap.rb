74b22e7945163e32f1ad57b5d88f7d40a79ad66d
Szymon Soltysiak <simon.soltysiak@ride.capital>
2025-10-08 22:35:24 +0200

    Phase 1

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,8 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "sortablejs" # @1.15.6
+pin "chart.js" # @4.5.0
+pin "@kurkle/color", to: "@kurkle--color.js" # @0.3.4
+pin "chartjs-plugin-dragdata" # @2.3.1
+pin "chart.js/helpers", to: "chart.js--helpers.js" # @4.5.0
